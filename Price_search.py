23b8a73648b3767c380527c695f84b32cabd31e8
Azman08022 <azmanarnob2000@gmail.com>
2021-09-04 01:50:03 +0600

    1st version(Raw code)

---
+++ Price_search.py
@@ -0,0 +1,19 @@
+apple= '12$' 
+banana='10$'
+lichi='9$'
+
+fruit=[apple,banana,lichi]
+
+f1,f2,f3=fruit
+
+Fruit=['Apple','Banana','Lichi']
+
+print(f1)
+print(f2)
+print(f3)
+
+print('Please choose your search item:')
+
+search=input()
+
+print(search))
