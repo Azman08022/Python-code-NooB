39e9761758013ba8ef873a2efb98c96220877c28
Azman08022 <azmanarnob2000@gmail.com>
2021-09-04 20:43:45 +0600

    2nd version(initialize products)

--- Price_search.py
+++ Price_search.py
@@ -1,19 +1,36 @@
-apple= '12$' 
-banana='10$'
-lichi='9$'
+
+phone=velorent=apple= '12$' 
+sim=cod=banana='10$'
+router=pubg=lichi='9$'
 
 fruit=[apple,banana,lichi]
-
 f1,f2,f3=fruit
 
-Fruit=['Apple','Banana','Lichi']
+games=[pubg,cod,velorent]
+g1,g2,g3=games
 
+electronics=[phone,sim,router]
+e1,e2,e3=electronics
+
+
+Fruit=['Apple','Banana','Lichi']
+F1,F2,F3=Fruit
+Games=['Pubg','Cod','Velorent']
+G1,G2,G3=Games
+Electronics=['phone','Sim','Router']
+E1,E2,E3=Electronics
+
+
+"""
 print(f1)
 print(f2)
 print(f3)
+"""
 
-print('Please choose your search item:')
+print("kindly choose Catagory :","\n"," "*25,"1.Fruit","\n"," "*25,"2.Games","\n"," "*25,"3.Electronics","\n","\n", )
 
+
+print('Please choose your search item:'," "*45)
 search=input()
 
-print(search)
+print("\n","\n","\n","\n",)
